7312b5aa93e7de9ca2f57ac51ce4197209df71d3
NovaPride <novapride27@gmail.com>
2024-04-05 16:04:37 +0300

    add task3 task

---
+++ task3/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const todoBtn = document.querySelector('.btn.btn_todo');
+
+  todoBtn.addEventListener('click', e => {
+    console.log(e);
+  });
+
+
+
+});
